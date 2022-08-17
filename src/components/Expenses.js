f7516c3bd2f6a46742c79549bc378254f550b529
Ophelia Montoya <ophelia_montoya@intuit.com>
2022-08-17 13:44:59 -0700

    first commit

---
+++ src/components/Expenses.js
@@ -0,0 +1,24 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css';
+
+function Expenses(props) {
+  const { expenses } = props;
+
+  return (
+    <div className="expenses">
+      {expenses.map((expense, index) => {
+        return (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Expenses;
